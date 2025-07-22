050c7293d1c7b048a3c1f823223b938f40f299ba
NadiaMHB <nhb_02@hotmail.com>
2025-07-21 19:33:38 -0500

    lalala

--- app/src/main/java/com/iot/stayflowdev/adminHotel/AdminChatClienteActivity.java
+++ app/src/main/java/com/iot/stayflowdev/adminHotel/AdminChatClienteActivity.java
@@ -64,7 +64,7 @@ public class AdminChatClienteActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         initializeFirebase();
-        initializeViews();alala
+        initializeViews();
         getIntentData();
         setupRecyclerView();
         setupClickListeners();
